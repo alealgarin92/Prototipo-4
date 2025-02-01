a52df7749d45ed98bda98dfe7572252a78c82ebe
ale4287 <alejandro.algarin@hotmail.com>
2025-02-01 04:06:46 -0300

    Update 3

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,17 +4,41 @@ using UnityEngine.Rendering;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject enemyPrefap;
+    [SerializeField] private GameObject powerupPrefap;
+    [SerializeField] private int enemyCount;
     private float spawnRange = 9.0f;
+    private int waveNumber = 1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Instantiate(enemyPrefap, GenerateSpawnPosition(), enemyPrefap.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        SpawnPowerup();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            SpawnPowerup();
+        }
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for(int i=0; i<enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefap, GenerateSpawnPosition(), enemyPrefap.transform.rotation);
+        }
+    }
+
+    void SpawnPowerup()
+    {
+        Instantiate(powerupPrefap, GenerateSpawnPosition(), powerupPrefap.transform.rotation);
     }
 
     private Vector3 GenerateSpawnPosition()
